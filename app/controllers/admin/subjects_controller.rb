161a3f12e252620f74a718f19779bf2c643d2ca6
Man Phan <phandinhman@gmail.com>
2016-08-11 14:07:07 +0700

    Edit and delete Subject

--- app/controllers/admin/subjects_controller.rb
+++ app/controllers/admin/subjects_controller.rb
@@ -1,5 +1,6 @@
 class Admin::SubjectsController < ApplicationController
   before_action :logged_in_user, :verify_admin
+  before_action :load_subject, except: [:index, :new, :create]
 
   def index
     @subjects = Subject.paginate page: params[:page]
@@ -20,9 +21,38 @@ class Admin::SubjectsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @subject.update_attributes subject_params
+      flash[:success] = t "flash.success.update_subject"
+      redirect_to admin_subjects_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @subject.destroy
+      flash[:success] = t "flash.success.delete_subject"
+    else
+      flash[:error] = t "flash.error.delete_subject"
+    end
+    redirect_to admin_subjects_path
+  end
+
   private
   def subject_params
     params.require(:subject).permit :name, :instructions,
       tasks_attributes: [:id, :name, :_destroy]
+  end
+
+  def load_subject
+    @subject = Subject.find_by id: params[:id]
+    unless @subject
+      flash[:danger] = t "admin.subjects.not_found"
+      redirect_to admin_subjects_path
+    end
   end
 end
