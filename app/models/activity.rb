7458b9da31c62ddeab23b37f276dbd2ac9e1bccc
hoangmirs <hoang.mirs@gmail.com>
2016-08-18 11:17:52 +0700

    View all histories of courses's activities

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,8 @@
 class Activity < ActiveRecord::Base
   belongs_to :user
+
+  scope :by_target, ->target_type, target_id do
+    where(target_type: target_type, target_id: target_id)
+      .order created_at: :desc
+  end
 end
