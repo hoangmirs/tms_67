6888446a4b9aab2ab1be27709e591b3f2d08ee34
hoangmirs <hoang.mirs@gmail.com>
2016-08-19 16:57:00 +0700

    Report day by day

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,9 @@ class Task < ActiveRecord::Base
 
   has_many :user_tasks
   has_many :users, through: :user_tasks
+
+  scope :by_user, ->user do
+    task_ids = "SELECT task_id FROM user_tasks WHERE  user_id = :user_id"
+    where("id IN (#{task_ids})", user_id: user.id)
+  end
 end
